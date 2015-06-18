359f24d136af8781e8e09e42cc3e33fbd2623fb2
Ehsan Tabari <ehsan@imac27.uncc.edu>
2015-06-18 13:43:05 -0400

    log enhanced

--- porthomclPairsOrthologs.py
+++ porthomclPairsOrthologs.py
@@ -136,6 +136,8 @@ if __name__ == '__main__':
 		if taxon1s < taxon2s:
 			taxon2_filename =  os.path.join(options.inBestHitFolder , taxon2s + '.bh.tsv')
 		
+			taxon2_index += 1
+			
 			if not os.path.exists(taxon2_filename):
 				log('{2} | Orthology | {0} | {1} | {3} | {4} MB | {5}'.format('3.' + str(taxon2_index) , 'ERROR: Targeted Best hit (bh file) Does not exists', options.index, taxon2s , memory_usage_resource(), datetime.now() ))
 				continue			
