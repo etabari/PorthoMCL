ed99220c1757f78121fa07f2f6b7d2db084052f2
Ehsan Tabari <ehsan@eBeast.carolina.rr.com>
2016-08-10 14:16:39 -0400

    removed BioPython requirement

--- porthomclSplitFasta.py
+++ porthomclSplitFasta.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python
 import sys, os
 from optparse import OptionParser
-from Bio import SeqIO
-import Bio
-
-
 
 options = None
 
@@ -16,35 +12,32 @@ if __name__ == '__main__':
 	parser = OptionParser(usage)
 	parser.add_option("-i", "--input", dest="input", help="input fasta")
 	parser.add_option("-o", "--outputFolder", dest="outputFolder", help="input fasta")
-	
-	parser.add_option("-n", "--minlen", dest="minlen", help="min acceptable length <= (default=0)", default=0, type=int)
-	parser.add_option("-x", "--maxlen", dest="maxlen", help="max acceptable length >= (default=-1: all)", default=-1, type=int)
+	# parser.add_option("-n", "--minlen", dest="minlen", help="min acceptable length <= (default=0)", default=0, type=int)
+	# parser.add_option("-x", "--maxlen", dest="maxlen", help="max acceptable length >= (default=-1: all)", default=-1, type=int)
 	parser.add_option("", "--numberFileNames", dest="numberFileNames", help="Generate numbered output files instead of TAXON.fasta", default=False, action='store_true')
-
-	
 	#parser.add_option("-s", "--filesize", dest="filesize", help="size of output files in number of seqs", type=int)
 	
 	
 	(options, args) = parser.parse_args()
 
 	if len(args) != 0 or not options.input or not options.outputFolder:
-		#print len(args)
 		parser.error("incorrect number of arguments.\n\t\tUse -h to get more information")
 	
-	#print options.filesize
+
 
 	file_count = 0
 	
 	output_handle = None 
 	output_taxon = None 
 
-	for record in SeqIO.parse(options.input, "fasta") :
-		seq_size = len(record.seq)
-		if seq_size >=options.minlen and (options.maxlen<0 or seq_size<=options.maxlen):
-
-			taxon = record.id.split('|')[0]
-
-
+	infile = open(options.input)
+	for line in infile:
+		if line[0]=='#':
+			continue
+		if line[0]=='>':
+			record_id = line[1:-1]
+			taxon = record_id.split('|')[0]
+			
 			if output_taxon != taxon:
 				file_count += 1
 				output_taxon = taxon
@@ -54,12 +47,10 @@ if __name__ == '__main__':
 					output_handle = open(os.path.join(options.outputFolder, str(file_count) + '.fasta'), "w")
 				else:
 					output_handle = open(os.path.join(options.outputFolder, output_taxon + '.fasta'), "w")
-
+				
 				print file_count, output_taxon
 
+		output_handle.write(line)
 
-			SeqIO.write([record], output_handle, 'fasta')
-			
-#	input_file.close()
 	output_handle.close()
 	print 'Done.'
